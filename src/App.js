9bd08d745f3fb608bd6f9c1c51065201c0ffb902
hariharan481 <118888824+hariharan481@users.noreply.github.com>
2023-11-14 17:28:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Add2 } from "./components/pages/Add2";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Add2 />
+    </div>
+  );
+};
+
+export default App;
